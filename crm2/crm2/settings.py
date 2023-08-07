2efdf61b2b3224eb9cbc734c4a71fb00e2dd0e8b
barzanzero90 <99289946+barzanzero90@users.noreply.github.com>
2023-08-07 15:36:03 -0700

    Update settings.py

--- crm2/crm2/settings.py
+++ crm2/crm2/settings.py
@@ -138,8 +138,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ramyardoski98@gmail.com'
-EMAIL_HOST_PASSWORD = 'RamyarDoski_98'
+EMAIL_HOST_USER = 'your_email'
+EMAIL_HOST_PASSWORD = 'your_password'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
